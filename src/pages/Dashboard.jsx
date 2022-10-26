01c6e8fa63df1a5a5877596a130ff4bc695f7b0f
tepiii <94727351+VallentinaStef@users.noreply.github.com>
2022-10-26 11:59:12 +0700

    sidebar & navbar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,9 +9,38 @@ export default function Dashboard() {
             <div className="container-fluid" style={{ margin: "0", padding: "0" }}>
                 <div className="d-flex">
                     <Sidebar />
-                    <Navbar />
 
+                    <div className="d-flex1">
+                        <Navbar />
+                        <div className="container-jumlah">
+                            <div className="container-jumlah-component">
+                                <div className="container-jumlah-component-kelas">
+                                    <div className="jumlah-alumni">
+                                        <h3 className="jumlah">Jumlah Alumni</h3>
+                                        <h2 className="orang">90</h2>
+                                    </div>
+                                    <div className="orang-alumni">
+                                        <h2 className="alumni">Alumni</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div className="container-bawah">
+                            <div className="container-diagram">
+                                <div className="container-diagram-bulat">
+
+                                </div>
+                                <div className="container-diagram-batang">
+
+                                </div>
+                            </div>
+                            <div className="container-keterserapan-alumni">
+
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
